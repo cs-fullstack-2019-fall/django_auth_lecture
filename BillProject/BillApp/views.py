7a81a502a5e2e0b5bf407adc0502dc1c883e708f
Kenn <kenn+git@code-crew.org>
2019-10-09 14:58:50 -0500

    Starter Code

--- BillProject/BillApp/views.py
+++ BillProject/BillApp/views.py
@@ -14,6 +14,7 @@ def login_user(request):
         loggedInUser = authenticate(username = request.POST['username'], password = request.POST["password"])
         print(loggedInUser)
         if loggedInUser is not None:
+            login(request, loggedInUser)
             return redirect("dashboard")
         else:
             return redirect("login_user")
