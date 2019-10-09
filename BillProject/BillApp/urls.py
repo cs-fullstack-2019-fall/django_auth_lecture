d55470f472edb7aa50af211293a0858ee67e713d
Kenn <kenn+git@code-crew.org>
2019-10-09 13:21:31 -0500

    Starter Code

--- BillProject/BillApp/urls.py
+++ BillProject/BillApp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('login_user/', views.login_user, name="login_user"),
+    path('logout_user/', views.logout_user, name="logout_user"),
+    path('new_user/', views.new_user, name="new_user"),
 ]
