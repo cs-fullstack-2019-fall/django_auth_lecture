88c98570722d3d314ac615bcc5766aef2e2525b1
Kenn <kenn+git@code-crew.org>
2019-10-09 14:22:06 -0500

    Starter Code

--- BillProject/BillApp/views.py
+++ BillProject/BillApp/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import render, redirect
 from .forms import UserForm
 
 # Create your views here.
 def index(request):
+    logout(request)
     return render(request, 'BillApp/index.html')
 
 def login_user(request):
@@ -17,7 +18,11 @@ def new_user(request):
     if request.method == "POST":
         newUserForm = UserForm(request.POST)
         if newUserForm.is_valid():
+
             User.objects.create_user(username= request.POST['username'], email = "", password = request.POST['password'])
+
+            loggedInUser = authenticate(username = request.POST['username'], password = request.POST['password'])
+            login(request, loggedInUser)
             return redirect("dashboard")
         else:    # Form is invalid
             context = {
