949740e7c8afb279a1881b88c9546cc36942c00a
Kenn <kenn+git@code-crew.org>
2019-10-09 13:29:19 -0500

    Starter Code

--- BillProject/BillApp/views.py
+++ BillProject/BillApp/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'BillApp/index.html')
+
+def login_user(request):
+    return render(request, "BillApp/login_user.html")
+
+def logout_user(request):
+    return render(request, "BillApp/index.html")
+
+def new_user(request):
+    return render(request, 'BillApp/new_user.html')
+
+def dashboard(request):
+    return render(request, "BillApp/dashboard.html")
